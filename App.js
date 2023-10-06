f88283e6c6e3ae0f58f20c33263edd8cb194175f
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-05 21:30:33 -0300

    começando o porjeto do spotcast

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Começando o app Spotcast, feito na aula do dev soutinho no youtube!</Text>
+      <Text>Bem-vindo ao Spotcast!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
